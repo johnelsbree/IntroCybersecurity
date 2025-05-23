3323b47cc8e93c3bef0595da75c7160795b7b781
John Elsbree <35622394+johnelsbree@users.noreply.github.com>
2025-05-22 23:24:54 -0700

    More output formatting changes

--- Lab9.3.1b/Login.py
+++ Lab9.3.1b/Login.py
@@ -4,28 +4,34 @@ import sqlite3
 con = sqlite3.connect('/home/kali/CourseFiles/IntroCybersecurity/chinook.db')
 cur = con.cursor()
 
+def underline(text):
+  return "\033[4m" + text + "\033[0m"
+
 def highlight(text):
-  return "\033[1;4m" + text + "\033[0m"
-  
+  return "\033[93;103m" + text + "\033[0m"
+
+def make_query(employeeID, password):
+  return "SELECT firstname, lastname FROM employees WHERE employeeID = '" + employeeID + "' AND password = '" + password + "'"
+
 # Prompt the user for login info
-print(highlight("Chinook Corp - Login"))
+print(underline("Chinook Corp - Login"))
 employeeID = input("EmployeeID: ")
 password = input("Password:   ")
 print()
 
 # Construct the SQL query
-query = "SELECT firstname, lastname FROM employees WHERE employeeID = '" + employeeID + "' AND password = '" + password + "'"
+query = make_query(employeeID, password)
 
 # Show the SQL query we are about to run
-print(highlight("SQLite will run this query:"))
-print(query)
+print(underline("SQLite will run this query:"))
+print(make_query(highlight(employeeID), highlight(password))
 print()
 
 # Fetch the result
 employee_info = (cur.fetchall())
 
 # Print the result
-print(highlight("SQLite query result:"))
+print(underline("SQLite query result:"))
 if employee_info:
   for x in employee_info:
     print(x)
